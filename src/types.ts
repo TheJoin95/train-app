194c9212631b52913e1059d3fdb5647a010ce7a5
Miki <miki.lombi@gmail.com>
2024-08-30 19:55:45 +0200

    types: update types

--- src/types.ts
+++ src/types.ts
@@ -16,3 +16,30 @@ export class JourneyPayload {
     this.departure = departure;
   }
 };
+
+export class TimeInfo {
+  stations: StationsMap;
+  date: Date;
+
+  constructor(stations: StationsMap, date: number) {
+    this.stations = stations;
+    this.date = new Date(date);
+  }
+};
+
+export type TimePreferences = {
+  outbound: string;
+  inbound: string;
+}
+
+export class Time {
+  outbound: TimeInfo;
+  inbound: TimeInfo;
+  preferences: TimePreferences;
+
+  constructor(outbound: TimeInfo, inbound: TimeInfo, preferences: TimePreferences) {
+    this.outbound = outbound;
+    this.inbound = inbound;
+    this.preferences = preferences;
+  }
+}
