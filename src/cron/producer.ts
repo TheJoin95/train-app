116491c41374f127504fd63b2a2484c18493d380
Miki <miki.lombi@gmail.com>
2024-09-02 22:39:29 +0200

    cron: update producer

---
+++ src/cron/producer.ts
@@ -0,0 +1,63 @@
+import type { StationsMap } from "@src/types";
+import sendToQueue, { QUEUES } from "@src/queue/sender";
+import { PrismaClient } from "@prisma/client";
+
+const DEPARTURE_STATION = 'Paris';
+const ARRIVAL_STATIONS = [
+  'Etretat',
+  'Lille',
+  'Londre',
+  'Lussemburgo',
+  'Munchen',
+  'Grenoble',
+  'Cassis',
+  'Marseille',
+  'Bezier',
+  'Carcassone',
+  'Pau',
+  'Barcellona',
+  'Toulose',
+  'Biarritz',
+  'Rennes',
+  'Brest',
+  'Saint Malo',
+  'Saint Nazaire',
+  'Nantes',
+  'Deauville',
+  'Dieppe',
+  'Amiens',
+];
+
+async function startProducer() {
+  try {
+    const prisma = new PrismaClient();
+    const times = await prisma.times.findMany({})
+    await prisma.$disconnect();
+
+    const formattedTimes = [];
+    times.forEach((time) => {
+      ['inbound', 'outbound'].forEach((mode) => {
+        formattedTimes.push({
+          stations: time[mode].stations,
+          departure: {
+            day: time[mode].date.getDay() + 1,
+            month: time[mode].date.getMonth() + 1,
+          },
+        });
+      })
+    });
+
+    for await (const payload of formattedTimes) {
+      await sendToQueue(QUEUES.QUEUE_GET_ROUTE_PRICE, payload);
+      await new Promise(resolve => setTimeout(resolve, 15000));
+    }
+  } catch (error) {
+    console.error(error);
+    //Retry after 3 second
+    setTimeout(() => {
+      startProducer();
+    }, 3000);
+  }
+}
+
+startProducer();
