8f8a21524c99c69cbdb83e20bb378b24fce3fead
Miki <miki.lombi@gmail.com>
2024-09-04 10:55:18 +0200

    mod: updated mod for runners

--- run-queue.sh
+++ run-queue.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-pkill -f train
-
 npm run worker:get-route-price > /home/ubuntu/get_route_price 2>&1 &
 npm run worker:save-route-price > /home/ubuntu/save_route_price 2>&1 &
 npm run worker:send-notification > /home/ubuntu/send_notification 2>&1 &
